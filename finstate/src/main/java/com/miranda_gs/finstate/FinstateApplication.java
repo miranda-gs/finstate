001d6c28d62b24de226916da0dad7604e5a55b4b
gabrielmirandasilv <gabrielmiranda61637@gmail.com>
2026-02-09 14:31:28 -0300

    SpringInitializr

---
+++ finstate/src/main/java/com/miranda_gs/finstate/FinstateApplication.java
@@ -0,0 +1,13 @@
+package com.miranda_gs.finstate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinstateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FinstateApplication.class, args);
+	}
+
+}
---
+++ finstate/src/test/java/com/miranda_gs/finstate/FinstateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.miranda_gs.finstate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinstateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
